7cf3f21419c7c1fc5070d893fe71aa07bdab42ce
Ville Vainio <ville.vainio@basware.com>
2025-04-15 18:13:52 +0300

    TICKET-123: Hello world again add functionality

--- hookmaster/cli.py
+++ hookmaster/cli.py
@@ -2,7 +2,8 @@ from argparse import ArgumentParser
 from pathlib import Path
 import shutil
 import subprocess
-
+import re
+import sys
 
 ROOT = Path(__file__).parent
 
@@ -17,18 +18,32 @@ def add_hooks_to_project(path: Path):
         for hook in simple_hooks:
             print(">", t)
             shutil.copy(hook, t)
- 
+
+
+def summary_line_for_branch(branch: str) -> str:
+    ticket = re.search(r"[A-Z]+-\d+", branch)
+    if not ticket:
+        return branch
+    ticket = ticket.group(0)
+    head, tail = branch.split(ticket)
+    if len(tail) == 0:
+        return ticket
+    
+    return f"{ticket}: {tail.strip().strip("-").replace("-", " ").capitalize()}"
+
 
 def prepare_commit_msg(current_message_file: Path):
     with open(current_message_file, "r") as f:
         message = f.read()
 
     branch = subprocess.run(["git", "symbolic-ref", "--short", "HEAD"], capture_output=True, text=True).stdout.strip()
-    # Modify the message as needed
-    modified_message = f"This is a hookmaster commit message. Branch {branch}\n\n" + message
+
+    summary = summary_line_for_branch(branch)
+    if not summary in message:
+        message = f"\n{summary}\n{message}"
 
     with open(current_message_file, "w") as f:
-        f.write(modified_message)
+        f.write(message)
 
 def main():
     parser = ArgumentParser(description="Hookmaster CLI")
@@ -39,6 +54,9 @@ def main():
 
     prep_commit_msg = subparsers.add_parser("prepare-commit-msg", help="Prepare commit message")
     prep_commit_msg.add_argument("current_message_file", help="Current commit message")
+    # you can ignore these arguments for most handlers
+    prep_commit_msg.add_argument("source", nargs='?', help="Source of the commit message: 'commit', 'merge' or 'squash'")
+    prep_commit_msg.add_argument("commit_object", nargs='?', help="Sha or whatever")
 
     parsed = parser.parse_args()
 
