f5b7b90c5a773b62e36d1ee0dc775ba8dd719d60
Ville Vainio <ville.vainio@basware.com>
2025-04-15 18:21:03 +0300

    format

--- hookmaster/__main__.py
+++ hookmaster/__main__.py
@@ -1,3 +1,5 @@
 from . import cli
+
+
 def main():
     cli.main()
--- hookmaster/cli.py
+++ hookmaster/cli.py
@@ -9,6 +9,7 @@ ROOT = Path(__file__).parent
 
 simple_hooks = list(ROOT.glob("hooks/*"))
 
+
 def add_hooks_to_project(path: Path):
     target_paths = path.glob("**/.git/hooks")
 
@@ -28,7 +29,7 @@ def summary_line_for_branch(branch: str) -> str:
     head, tail = branch.split(ticket)
     if len(tail) == 0:
         return ticket
-    
+
     return f"{ticket}: {tail.strip().strip("-").replace("-", " ").capitalize()}"
 
 
@@ -36,7 +37,9 @@ def prepare_commit_msg(current_message_file: Path):
     with open(current_message_file, "r") as f:
         message = f.read()
 
-    branch = subprocess.run(["git", "symbolic-ref", "--short", "HEAD"], capture_output=True, text=True).stdout.strip()
+    branch = subprocess.run(
+        ["git", "symbolic-ref", "--short", "HEAD"], capture_output=True, text=True
+    ).stdout.strip()
 
     summary = summary_line_for_branch(branch)
     if not summary in message:
@@ -45,6 +48,7 @@ def prepare_commit_msg(current_message_file: Path):
     with open(current_message_file, "w") as f:
         f.write(message)
 
+
 def main():
     parser = ArgumentParser(description="Hookmaster CLI")
     subparsers = parser.add_subparsers(dest="command")
@@ -52,11 +56,17 @@ def main():
     add_cmd = subparsers.add_parser("add", help="Add hooks to a project")
     add_cmd.add_argument("path", help="Project path or directory")
 
-    prep_commit_msg = subparsers.add_parser("prepare-commit-msg", help="Prepare commit message")
+    prep_commit_msg = subparsers.add_parser(
+        "prepare-commit-msg", help="Prepare commit message"
+    )
     prep_commit_msg.add_argument("current_message_file", help="Current commit message")
     # you can ignore these arguments for most handlers
-    prep_commit_msg.add_argument("source", nargs='?', help="Source of the commit message: 'commit', 'merge' or 'squash'")
-    prep_commit_msg.add_argument("commit_object", nargs='?', help="Sha or whatever")
+    prep_commit_msg.add_argument(
+        "source",
+        nargs="?",
+        help="Source of the commit message: 'commit', 'merge' or 'squash'",
+    )
+    prep_commit_msg.add_argument("commit_object", nargs="?", help="Sha or whatever")
 
     parsed = parser.parse_args()
 
@@ -68,4 +78,3 @@ def main():
         add_hooks_to_project(path)
     elif parsed.command == "prepare-commit-msg":
         prepare_commit_msg(Path(parsed.current_message_file))
-
